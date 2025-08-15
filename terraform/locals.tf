05e26ba4e26e89e3b59cedbfe6a6bf91bb9e2746
Mohd Zeeshan <mohdzeeshan@Mohds-MacBook-Pro.local>
2025-08-15 22:24:02 +0530

    fixes-2

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,16 +1,16 @@
 locals {
   # Short codes for regions (extend as needed)
   location_short = lookup({
-    eastus     = "eus"
-    eastus2    = "eus2"
-    westus     = "wus"
-    westus2    = "wus2"
-    westus3    = "wus3"
-    centralus  = "cus"
-    northeurope = "neu"
-    westeurope  = "weu"
-    uksouth     = "uks"
-    ukwest      = "ukw"
+    eastus        = "eus"
+    eastus2       = "eus2"
+    westus        = "wus"
+    westus2       = "wus2"
+    westus3       = "wus3"
+    centralus     = "cus"
+    northeurope   = "neu"
+    westeurope    = "weu"
+    uksouth       = "uks"
+    ukwest        = "ukw"
     southeastasia = "sea"
   }, lower(var.location), lower(var.location))
 
@@ -23,4 +23,10 @@ locals {
   # Enable Service Endpoints on subnets based on mode toggles
   se_webapp_enabled = var.webapp_network_mode == "service_endpoint"
   se_sql_enabled    = var.sql_network_mode == "service_endpoint"
+
+  # Default tags to apply across resources (use: tags = merge(local.default_tags, { ... }))
+  default_tags = {
+    environment = var.environment
+    org         = var.org_prefix
+  }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,4 @@
 provider "azurerm" {
   features {}
 
-  # Optional: set default tags across all resources
-  default_tags {
-    tags = {
-      environment = var.environment
-      org         = var.org_prefix
-    }
-  }
 }
